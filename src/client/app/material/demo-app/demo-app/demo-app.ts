27772f2f4b2705056ea0d0d383a17799f20e50a0
Kunal Kamble <dev@mymac.ad1.peelsb.com>
2018-11-22 14:20:53 -0500

    fix app layout issue.

--- src/client/app/material/demo-app/demo-app/demo-app.ts
+++ src/client/app/material/demo-app/demo-app/demo-app.ts
@@ -6,34 +6,34 @@ import { Component, ViewEncapsulation } from '@angular/core';
   template: `
   <div fxLayout="row">
     <nav fxLayout='row' fxLayoutAlign="center center">
-      <a [routerLink]="['autocomplete']">Autocomplete</a>
-      <a [routerLink]="['button']">Button</a>
-      <a [routerLink]="['button-toggle']">Button Toggle</a>
-      <a [routerLink]="['card']">Card</a>
-      <a [routerLink]="['checkbox']">Checkbox</a>
-      <a [routerLink]="['dialog']">Dialog</a>
-      <a [routerLink]="['gestures']">Gestures</a>
-      <a [routerLink]="['grid-list']">Grid List</a>
-      <a [routerLink]="['icon']">Icon</a>
-      <a [routerLink]="['input']">Input</a>
-      <a [routerLink]="['list']">List</a>
-      <a [routerLink]="['menu']">Menu</a>
-      <a [routerLink]="['live-announcer']">Live Announcer</a>
-      <a [routerLink]="['overlay']">Overlay</a>
-      <a [routerLink]="['portal']">Portal</a>
-      <a [routerLink]="['progress-bar']">Progress Bar</a>
-      <a [routerLink]="['progress-circle']">Progress Circle</a>
-      <a [routerLink]="['radio']">Radio</a>
-      <a [routerLink]="['ripple']">Ripple</a>
-      <a [routerLink]="['sidenav']">Sidenav</a>
-      <a [routerLink]="['slider']">Slider</a>
-      <a [routerLink]="['slide-toggle']">Slide Toggle</a>
-      <a [routerLink]="['tabs']">Tabs</a>
-      <a [routerLink]="['toolbar']">Toolbar</a>
-      <a [routerLink]="['tooltip']">Tooltip</a>
-      <a [routerLink]="['tree']">Tree</a>
-      <a [routerLink]="['baseline']">Baseline</a>
-      <a [routerLink]="['table']">Table</a>
+      <a mat-button [routerLink]="['autocomplete']">Autocomplete</a>
+      <a mat-button [routerLink]="['button']">Button</a>
+      <a mat-button [routerLink]="['button-toggle']">Button Toggle</a>
+      <a mat-button [routerLink]="['card']">Card</a>
+      <a mat-button [routerLink]="['checkbox']">Checkbox</a>
+      <a mat-button [routerLink]="['dialog']">Dialog</a>
+      <a mat-button [routerLink]="['gestures']">Gestures</a>
+      <a mat-button [routerLink]="['grid-list']">Grid List</a>
+      <a mat-button [routerLink]="['icon']">Icon</a>
+      <a mat-button [routerLink]="['input']">Input</a>
+      <a mat-button [routerLink]="['list']">List</a>
+      <a mat-button [routerLink]="['menu']">Menu</a>
+      <a mat-button [routerLink]="['live-announcer']">Live Announcer</a>
+      <a mat-button [routerLink]="['overlay']">Overlay</a>
+      <a mat-button [routerLink]="['portal']">Portal</a>
+      <a mat-button [routerLink]="['progress-bar']">Progress Bar</a>
+      <a mat-button [routerLink]="['progress-circle']">Progress Circle</a>
+      <a mat-button [routerLink]="['radio']">Radio</a>
+      <a mat-button [routerLink]="['ripple']">Ripple</a>
+      <a mat-button [routerLink]="['sidenav']">Sidenav</a>
+      <a mat-button [routerLink]="['slider']">Slider</a>
+      <a mat-button [routerLink]="['slide-toggle']">Slide Toggle</a>
+      <a mat-button [routerLink]="['tabs']">Tabs</a>
+      <a mat-button [routerLink]="['toolbar']">Toolbar</a>
+      <a mat-button [routerLink]="['tooltip']">Tooltip</a>
+      <a mat-button [routerLink]="['tree']">Tree</a>
+      <a mat-button [routerLink]="['baseline']">Baseline</a>
+      <a mat-button [routerLink]="['table']">Table</a>
     </nav>
   </div>
   <router-outlet></router-outlet>
